2b772a767b63001308a991324318ec703b5ff5ce
Colin Fay <colin@thinkr.fr>
2020-10-27 16:16:16 +0100

    Update R/APIres21rdf.R

--- R/APIres21rdf.R
+++ R/APIres21rdf.R
@@ -9,6 +9,28 @@
 APIres21rdf <- function (url){
   res <- httr::GET(url)
   df <- httr::content(res)[[1]] # <- Turn this into a data.frame with one row
-  dfout <- data.frame(unlist(df))
+ dfout <- tibble::tibble(
+  id= df$id,
+  name= df$name,
+  tagline= df$tagline,
+  first_brewed= df$first_brewed,
+  description= df$description,
+  image_url= df$image_url,
+  abv= df$abv,
+  ibu= df$ibu,
+  target_fg= df$target_fg,
+  target_og= df$target_og,
+  ebc= df$ebc,
+  srm= df$srm,
+  ph= df$ph,
+  attenuation_level= df$attenuation_level,
+  volume= list(df$volume),
+  boil_volume= list(df$boil_volume),
+  method= list(df$method),
+  ingredients= list(df$ingredients),
+  food_pairing= list(df$food_pairing),
+  brewers_tips= df$brewers_tips,
+  contributed_by= df$contributed_by
+)
   return(dfout)
 }
