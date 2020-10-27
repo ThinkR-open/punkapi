5081854143514b004acf3fa0b5a0caa8224e2366
colin <colin@thinkr.fr>
2020-10-27 21:28:14 +0100

    Clean version before next training

--- R/check_results.R
+++ R/check_results.R
@@ -3,16 +3,18 @@
 #' @param requete requete sur l'API punk
 #'
 #' @return un booleen (status = 200 : TRUE / FALSE)
-#' @export
+
 #' @importFrom httr status_code
 #' @importFrom assertthat assert_that
 #'
+#' @noRd
+#'
 #' @examples
-#' requete <- httr::GET("https://api.punkapi.com/v2/beers/1")
+#' requete <- httr::GET("https://api.punkapi.com/v2/beers/789456123")
 #' check_results(requete = requete)
 check_results <- function(requete) {
-
-  assert_that(status_code(requete) == 200)
-
-
+  assert_that(
+    status_code(requete) == 200,
+    msg = "The API request returned an error"
+  )
 }
