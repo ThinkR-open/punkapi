458fc2760d8cfd31ddb447f48c99c2144740868c
Fabian Lenartz <f.lenartz@issep.be>
2020-10-29 13:37:44 +0000

    First commit for issue 11

---
+++ R/punk_by_ebc.R
@@ -0,0 +1,37 @@
+#' A function that returns a list of beers based on the ebc
+#'
+#' @param ebcmin
+#' @param ebcmax
+#'
+#' @return a list of beers with ebc larger than ebcmin and less than ebcmax
+#' @export
+#' @import assertthat
+#'
+#' @examples
+#' out <- punk_by_ebc(12,10)
+punk_by_ebc <- function(ebcmin=5, ebcmax=10){
+  assert_that(
+    is.numeric(ebcmin),
+    msg = "ebcmin should be a number"
+  )
+  assert_that(
+    is.numeric(ebcmax),
+    msg = "ebcmin should be a number"
+  )
+  assert_that(
+    ebcmin>=0,
+    msg = "ebcmin should be larger or equal to zero"
+  )
+  assert_that(
+    ebcmin<=100,
+    msg = "ebcmax should be less or equal to 100"
+  )
+  assert_that(
+    ebcmin<=ebcmax,
+    msg = "ebcmin should be less or equal to ebcmax"
+  )
+  res <- httr::GET("https://api.punkapi.com/v2/beers",
+                   query = list("ebc_gt" = as.character(ebcmin),
+                                "ebc_lt" = as.character(ebcmax)))
+  return(res)
+}
