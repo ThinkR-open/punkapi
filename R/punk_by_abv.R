9294d188bdbc2f617e12f716c7364ffab27b06c5
laurent spanu <l.spanu@issep.be>
2020-10-29 14:12:14 +0000

    issue #9 fix error in punk_by_abv

--- R/punk_by_abv.R
+++ R/punk_by_abv.R
@@ -5,6 +5,7 @@
 #'
 #' @return a tibble
 #' @export
+#' @import purrr
 #'
 #' @examples
 #' punk_by_abv(abv_gt = "3", abv_lt = "6")
@@ -14,11 +15,14 @@ punk_by_abv <- function(abv_gt, abv_lt) {
   assert_that(is_character(abv_gt))
   assert_that(is_character(abv_lt))
 
-  check_results(res)
+
 res <- httr::GET(
     "https://api.punkapi.com/v2/beers",
     query = list("abv_gt" = abv_gt, "abv_lt" = abv_lt)
   )
+
+check_results(res)
+
 ct <- content(res)[[1]]
 
 
